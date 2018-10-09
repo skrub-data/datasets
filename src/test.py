3e2ae8623210489cc376c6e17b705dc1d1cb479b
CUNY Maxime <maxime.cuny@inria.fr>
2018-10-09 09:13:10 +0200

    Multithreaded the test script

--- src/test.py
+++ src/test.py
@@ -1,45 +1,41 @@
 from colleges import get_colleges_df
 from crime_data import get_crime_df
-from open_payments import get_open_payment_df
 from employee_salaries import get_employee_salaries_df
 from journal_influence import get_journal_influence_df
 from medical_charge import get_medical_charge_df
 from met_objects import get_met_objects_df
 from midwest_survey import get_midwest_survey_df
+from open_payments import get_open_payment_df
 from road_safety import get_road_safety_df
 from traffic_violations import get_traffic_violations_df
 
-# import os
-# import shutil
-# TODO: At least do a dummy test and check if files are downloaded and outputs generated
+import asyncio
 
-print("Getting traffic violations")
-get_traffic_violations_df()
+func_map = {
+    'crime': get_crime_df,
+    'colleges': get_colleges_df,
+    'employee': get_employee_salaries_df,
+    'journal': get_journal_influence_df,
+    'medical': get_medical_charge_df,
+    'met': get_met_objects_df,
+    'midwest': get_midwest_survey_df,
+    'road': get_road_safety_df,
+    'traffic': get_traffic_violations_df,
+    'open payment': get_open_payment_df
+}
 
-print("Getting road safety")
-get_road_safety_df()
 
-print("Getting midwest survey")
-get_midwest_survey_df()
+async def run(name, func):
+    print('Getting', name.upper(), 'data')
+    df = func()
+    assert (df is not None)
 
-print("Getting met objects")
-get_met_objects_df()
 
-print("Getting medical charge")
-get_medical_charge_df()
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+tasks = [
+    asyncio.ensure_future(run(f, func_map[f])) for f in func_map
+]
 
-print("Getting journal influence")
-get_journal_influence_df()
-
-print("Getting employee salaries")
-get_employee_salaries_df()
-
-print("Getting open payment")
-get_open_payment_df()
-
-print("Getting crime")
-get_crime_df()
-
-print("Getting colleges")
-get_colleges_df()
-
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
